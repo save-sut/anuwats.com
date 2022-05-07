32d526f9295cc7187360aef181c96a933ad2503b
save-sut <anuwat_song@outlook.com>
2022-05-08 01:35:32 +0700

    Fixed single qoute character

--- components/page-not-found/index.tsx
+++ components/page-not-found/index.tsx
@@ -6,7 +6,7 @@ const PageNotFound = () => {
           <div className="px-4 text-2xl text-gray-500 border-r border-gray-400 tracking-wider">404</div>
           <div>
             <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">Page Not Found</div>
-            <div className="ml-4 text-xs text-gray-500 uppercase tracking-wider">The page you're looking for doesn't exist</div>
+            <div className="ml-4 text-xs text-gray-500 uppercase tracking-wider">The page you&lsquo;re looking for doesn&lsquo;t exist</div>
           </div>
         </div>
       </div>
